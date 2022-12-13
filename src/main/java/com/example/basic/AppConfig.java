c2f1ce1154c52be42f4cf1b6fb24a86cd3410c98
sys970717 <sys970717@naver.com>
2022-12-13 09:46:21 +0900

    불필요 인터페이스 선언된 내용 삭제

--- src/main/java/com/example/basic/AppConfig.java
+++ src/main/java/com/example/basic/AppConfig.java
@@ -1,5 +1,6 @@
 package com.example.basic;
 
+import com.example.basic.hello.core.discount.DiscountPolicy;
 import com.example.basic.hello.core.discount.FixDiscountPolicy;
 import com.example.basic.hello.core.discount.RateDiscountPolicy;
 import com.example.basic.hello.core.order.OrderService;
@@ -9,12 +10,21 @@ import com.example.basic.member.MemberServiceImpl;
 import com.example.basic.member.MemoryMemberRepository;
 
 public class AppConfig {
+    private MemoryMemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+
+    public DiscountPolicy discountPolicy() {
+//        return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
+    }
+
     public MemberService memberService () {
         // 생성자 주입
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new RateDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 }
